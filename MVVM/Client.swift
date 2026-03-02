24b5ab42e35cbd288eadc283af9ee2cc7f4413ea
Praveen S <sashapraveen@gmail.com>
2026-03-02 22:33:19 +0530

    added status property

--- MVVM/Client.swift
+++ MVVM/Client.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Client {
-    
+    var status : String = "pending"
     func fetchDataFromApi () async throws -> DataModel {
         let initialurl = URL(string: "https://cdn.jwplayer.com/v2/playlists/GJpN5tES")
         guard let url = initialurl else {
